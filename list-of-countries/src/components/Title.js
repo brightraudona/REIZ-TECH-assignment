67173a231c0434088207b6be1a23ad7c36c8885e
Matas Togobickij <matas.togobickij@ktu.edu>
2022-02-01 20:45:36 +0200

    changed titele

--- list-of-countries/src/components/Title.js
+++ list-of-countries/src/components/Title.js
@@ -2,7 +2,7 @@
 export default function Title() {
     return (
         <div className="Title pb-5">
-            <h1>REIZ TECH homework assignment document for frontend developer intern position</h1>
+            <h1>REIZ TECH homework assignment for frontend developer intern position</h1>
         </div>
     )
 }
